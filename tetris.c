6df2d35ad97fa7026aa8f463d1ee46f5ca096440
HectorID <hectormargittay@live.com>
2016-04-04 04:53:58 -0300

    Falta consertar um bug que ocorre quando a tecla 'S' é pressionada.

--- tetris.c
+++ tetris.c
@@ -1,9 +1,11 @@
-//	Trabalho Pratica 1                            |
-//	Genaina - Programacao Sistematica             |
-//	André Luiz de Moura Ramos Bittencourt         |
-//	Claudio Segala Rodrigues Silva Filho          |
-//	Anne Caroline de Assis Pereira                |
-//	Hector Rocha Margittay                        |
+//	Programacao Sistematica	                      |
+//	Trabalho Pratico 1                            |
+//	Professor(a): Genaina                         |
+//	Alunos:                                       |
+//		André Luiz de Moura Ramos Bittencourt     |
+//		Claudio Segala Rodrigues Silva Filho      |
+//		Anne Caroline de Assis Pereira            |
+//		Hector Rocha Margittay                    |
 // _______________________________________________|
 
 
@@ -19,14 +21,22 @@
 // Defines ------------------------------------------------------------------------------------------------------------------------------------------
 
 #define RESET "\x1b[0m"
-#define KNRM  "\x1b[0m"
-#define KRED  "\x1b[31m"
-#define KGRN  "\x1b[32m"
-#define KYEL  "\x1b[33m"
-#define KBLU  "\x1b[34m"
-#define KMAG  "\x1b[35m"
-#define KCYN  "\x1b[36m"
-#define KWHT  "\x1b[37m"
+#define KBLC "\x1b[30m"
+#define KRED "\x1b[31m"
+#define KGRN "\x1b[32m"
+#define KYEL "\x1b[33m"
+#define KBLU "\x1b[34m"
+#define KMAG "\x1b[35m"
+#define KCYN "\x1b[36m"
+#define KWHT "\x1b[37m"
+#define BGBLC "\x1b[40m"
+#define BGRED "\x1b[41m"
+#define BGGRN "\x1b[42m"
+#define BGYEL "\x1b[43m"
+#define BGBLU "\x1b[44m"
+#define BGMAG "\x1b[45m"
+#define BGCYN "\x1b[46m"
+#define BGWHT "\x1b[47m"
 #define LARGURA 15
 #define ALTURA 25
 #define LIMITE 5
@@ -53,46 +63,13 @@ int next_round = 1;
 
 // Funcoes -------------------------------------------------------------------------------------------------------------------------------------------
 
+// MODULO TELA________________________________________________________________________________________________________________________________________
+
 void limpa_tela_legal(){
 	system("clear || cls");
 }
 
-void limpa_buffer(){
-  while(getchar()!='\n');
-}
-
-int getch(void) {
-  int ch;
-  struct termios oldt;
-  struct termios newt;
-  tcgetattr(STDIN_FILENO, &oldt); /*store old settings */
-  newt = oldt; /* copy old settings to new settings */
-  newt.c_lflag &= ~(ICANON | ECHO); /* make one change to old settings in new settings */
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt); /*apply the new settings immediatly */
-  ch = getchar(); /* standard getchar call */
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt); /*reapply the old settings */
-  return ch; /*return received char */
-}
-
-char getch_char(void) {
-  int ch = getch();
-  if(ch >= 0 && ch < 128)
-	  return (char) ch;
-  return 0;
-}
-
-void enter(){
-  printf(" Aperte [enter] para ir ao MENU ");
-  limpa_buffer();
-  limpa_tela_legal();
-}
-
-int valido(char opt){
-  if(opt == 'a' || opt == 'A' || opt == 's' || opt == 'S' || opt == 'd' || opt == 'D') return 0;
-  return 1;
-}
-
-void tela_inicio(){
+void show_title(){
 	limpa_tela_legal();
 	printf(KBLU "______________  ______________  ______________  ___________    __________  ______________ \n" RESET);
 	printf(KBLU "      |         |                     |         |          \\       ||      |              \n" RESET);
@@ -102,10 +79,62 @@ void tela_inicio(){
 	printf(KBLU "      |         |                     |         |          \\       ||                   | \n" RESET);
 	printf(KBLU "      |         |_____________        |         |           \\  ____||____   ____________| \n" RESET);
 	printf("\n\n");
-	usleep(1500000);
 }
 
-void tela_fim(){
+void show_menu() {
+	limpa_tela_legal();
+	printf(KBLU "1 - Jogar\n" RESET);
+	printf(KBLU "2 - Sair\n" RESET);
+}
+
+void show_instructions(){
+	limpa_tela_legal();
+	printf(" Use as teclas [A], [S] e [D] (pode usar CapsLock) para mover as pecas\n\n");
+	printf("                  Aperte [ENTER] para ir ao MENU\n");
+}
+
+void show_field(){
+	int x, y;
+	limpa_tela_legal();
+    printf(" Pontuacao: %d\n\n", game.points);
+    for(x = 0; x < LARGURA+2; x++)
+    	printf(BGWHT "  ");
+    printf(RESET "\n");
+    for(y = 0; y < ALTURA; y++){
+    	printf(BGWHT "  ");
+		for(x = 0; x < LARGURA; x++) {
+			if(game.field[x][y] == VAZIO) {
+				if(y == LIMITE)	
+					printf(KWHT BGBLC "--");
+				else 
+					printf(BGBLC "  ");
+			}
+			else {
+				switch(currentBlock.color) {
+					case 1:	printf(KRED);
+							break;
+					case 2:	printf(KGRN);
+							break;
+					case 3:	printf(KYEL);
+							break;
+					case 4:	printf(KBLU);
+							break;
+					case 5:	printf(KMAG);
+							break;
+					case 6:	printf(KCYN);
+							break;
+				}
+				printf(BGBLC "[]");
+			}
+		}
+		printf(BGWHT "  \n" RESET);
+    }
+    for(x = 0; x < LARGURA+2; x++)
+    	printf(BGWHT "  ");
+    printf(RESET "\n");
+}
+
+void show_end(){
 	limpa_tela_legal();
 	printf(KRED "Vc pontuou %d em %lf\n"  RESET, game.points, game.duration);
 	printf(KGRN "Parabens por ter chegado tao longe jovem padawan\n" RESET);
@@ -117,37 +146,63 @@ void tela_fim(){
 	usleep(500000);
 }
 
-void show_instructions(){
-	limpa_tela_legal();
-	printf(" Use as teclas a, s e d (pode usar CapsLock) para mover as pecas\n");
-	usleep(4000000);
-}
- 
-//MODULO BLOCO___________________________________________________________________________________________
-int obstaculo(int deslocax, int deslocay) {
-	int i, j, x, y, ax, ay, cx, cy;
-	
-	x = currentBlock.x;
-	y = currentBlock.y;
+// MODULO BLOCO_______________________________________________________________________________________________________________________________________
 
-	if(currentBlock.direction==HORIZONTAL) {
-		ax = 1;
-		ay = 0;
+void put_block(int newX, int newY) {
+	int i, dirX, dirY;
+	
+	if(currentBlock.direction == HORIZONTAL) {
+		dirX = 1;
+		dirY = 0;
 	}
-	else if(currentBlock.direction==VERTICAL) {
-		ax = 0;
-		ay = 1;
+	else if(currentBlock.direction == VERTICAL) {
+		dirX = 0;
+		dirY = 1;
+	}
+	
+	for(i = 0; i < currentBlock.size; i++)
+		game.field[currentBlock.x + (dirX * i)][currentBlock.y + (dirY * i)] = VAZIO;
+		
+	for(i = 0; i < currentBlock.size; i++)
+		game.field[newX + (dirX * i)][newY + (dirY * i)] = currentBlock.color;
+}
+
+void new_block(){
+	int c = rand()%6;
+
+	while(c == currentBlock.color) c = rand()%6;
+	currentBlock.color = c;
+	currentBlock.direction = rand() % 2;
+	currentBlock.size = rand() % 3 + 3;
+	currentBlock.y = 0;
+	currentBlock.x = rand()%LARGURA-(currentBlock.direction?0:currentBlock.size);
+	put_block(currentBlock.x, currentBlock.y);
+}
+
+int collision(int deslocX, int deslocY) {
+	int i, j, origX, origY, dirX, dirY, x, y, check;
+	
+	origX = currentBlock.x;
+	origY = currentBlock.y;
+
+	if(currentBlock.direction == HORIZONTAL) {
+		dirX = 1;
+		dirY = 0;
+	}
+	else if(currentBlock.direction == VERTICAL) {
+		dirX = 0;
+		dirY = 1;
 	}
 
 	for(i = 0 ; i < currentBlock.size ; i++) {
-		cx = x + (ax * i) + deslocax
-		cy = y + (ay * i) + deslocay
-		if(cx >= 0 && cx < LARGURA &&
-		   cy >= 0 && cy < ALTURA 0) {
-			if(game.field[cx][cy] != VAZIO) {
+		x = origX + (dirX * i) + deslocX;
+		y = origY + (dirY * i) + deslocY;
+		if(x >= 0 && x < LARGURA &&
+		   y >= 0 && y < ALTURA) {
+			if(game.field[x][y] != VAZIO) {
 				check = 0;
 				for(j = 0 ; j < currentBlock.size ; j++) {
-					if(cx == x + (ax*j) && cy == y + (ay*j))
+					if(x == origX + (dirX * j) && y == origY + (dirY * j))
 						check = 1;
 				}
 				if(check == 0)
@@ -161,123 +216,124 @@ int obstaculo(int deslocax, int deslocay) {
 	return 0;
 }
 
-void move(char opt){
-	if(opt == 's' || opt == 'S'){ /* move para baixo */ 
-		obstaculo(0, 1);	
+void move_block(char opt){
+	int newX = currentBlock.x, newY = currentBlock.y;
+	
+	if(opt == 'S'){ /* move para baixo */ 
+		while(!collision(0, 1))
+			newY++;	
 	}
-	else if(opt == 'd' || opt == 'D'){ /* move para direita */ 
-		obstaculo(1, 0);	
+	else if(opt == 'D'){ /* move para direita */ 
+		if(!collision(1, 0))
+			newX++;	
 	}
-	else if(opt == 'a' || opt == 'A'){ /* move para esquerda */ 
-		obstaculo(-1, 0);	
+	else if(opt == 'A'){ /* move para esquerda */ 
+		if(!collision(-1, 0))
+			newX--;	
+	}
+	
+	if(newX != currentBlock.x || newY != currentBlock.y) {
+		put_block(newX, newY);
+		currentBlock.x = newX;
+		currentBlock.y = newY;	
 	}
 }
 
 void cut_this_line(int y){
 	int x;
-	for(;y>=LIMITE;y--)
+	for(;y>=1;y--)
 		for(x=0;x<LARGURA;x++)
 			game.field[x][y]=game.field[x][y-1];
 }
 
-
-int any_complete_line(){
-	int x, y, p = 0, cnt;
+void any_complete_line(){
+	int x, y, cnt;
 	for(y = LIMITE; y < ALTURA; y++){
 		cnt = 0;
 		for(x = 0; x < LARGURA; x++)
-			if(game.field[x][y] == '1') cnt += 1;
-		if(cnt == LARGURA){	/* confere se o contador tem o tamanho da linha maxima*/
+			if(game.field[x][y] != VAZIO) cnt += 1;
+		if(cnt == LARGURA) {	/* confere se o contador tem o tamanho da linha maxima*/
 			cut_this_line(y);
-			p += 100;
+			game.points += 100;
 		}
 	}
-	fall('s');
-	return p;
 }
 
-//MODULO TELA________________________________________________________________________________________
+// MODULO ENGINE______________________________________________________________________________________________________________________________________
 
-void show_field(){
-	int x, y, i;
-	limpa_tela_legal();
-    printf(" Pontuacao: %d\n\n", game.points++);
-    for(y = 0; y < ALTURA; y++){
-    	if(i == LIMITE)	printf(KRED "-------------------------------------------------------------\n" RESET);
-        else 		printf(KYEL "-------------------------------------------------------------\n" RESET);
-        for(x = 0; x < LARGURA; x++){
-        	if(game.field[x][y] == 0)
-            	printf(KYEL "|" RESET KCYN "   " RESET);
-            else
-            	printf(KYEL "|" RESET KCYN " * " RESET);
-        }
-        printf(KYEL "|\n");
-    }
-    printf("-------------------------------------------------------------\n" RESET);
-    usleep(10000);
+void start() {
+	show_title();
+	usleep(1500000);
 }
 
-// MODULO ENGINE_________________________________________________________________________________
+char getch(void) {
+	int ch;
+	struct termios oldt;
+	struct termios newt;
+	tcgetattr(STDIN_FILENO, &oldt); /*store old settings */
+	newt = oldt; /* copy old settings to new settings */
+	newt.c_lflag &= ~(ICANON | ECHO); /* make one change to old settings in new settings */
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt); /*apply the new settings immediatly */
+	ch = getchar(); /* standard getchar call */
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt); /*reapply the old settings */
+	return ch; /*return received char */
+}
 
-void prepare_field(){
-	memset(game.field, 0, sizeof(int)*LARGURA*ALTURA);
+char read_input() {
+	char ch;
+	
+	ch = getch();
+	fflush(stdin);
+	if(ch >= 'a' && ch <= 'z')
+		ch -= 'a' - 'A';
+	return ch;
+}
+
+void set_game(){
+	srand(time(NULL));
+	memset(game.field, VAZIO, sizeof(int)*LARGURA*ALTURA);
 	game.points = 0;
 }
 
-int gameOver(){
-	if(currentBlock.y < LIMITE) return 0;
-	return 1;
-}
-
-void putBlock(){
-	currentBlock.color = rand()%4;
-	currentBlock.direction = rand() % 2;
-	currentBlock.size = rand() % 3 + 3;
-	currentBlock.y = 0;
-	currentBlock.x = rand()%LARGURA-(currentBlock.position?0:currentBlock.size);
-	/* implementar desenha na matriz */
-}
-
-char input() {
-	char opt;
-	do { opt = getch_char(); }
-	while( valido(opt) );
-	return opt;
+int game_over(){
+	if(currentBlock.y < LIMITE) return 1;
+	return 0;
 }
 
 void game_on(){
 	clock_t t_begin = clock() / CLOCKS_PER_SEC, t_end;
-	prepare_field();
+	char input;
+	
+	set_game();
 	show_instructions();
 	do{
-		if(next_round == 1){
-			putBlock(); 
-			next_round = 0;
-		}
-		limpa_tela_legal();
+		new_block();
+		do {
+			show_field();	printf("%d x %d\n", currentBlock.x, currentBlock.y);
+			input = read_input();
+			move_block(input);
+		} while(input != 'S');
 		show_field();
-		move(input());
-		usleep(800000);
-		game.points += any_complete_line();
-	}while(gameOver(););
+		any_complete_line(); putchar('!');
+	} while(!game_over());
 	t_end = clock() / CLOCKS_PER_SEC;
 	/* De acordo com o dito pelo IME em uma de suas apostilas, esse metodo so consegue guardar ate 35 minutos*/
 	game.duration = t_end - t_begin;
+	show_end();
 }
 
-int main(){
-	srand( time(NULL) );
-	tela_inicio();
-	char a;
-	printf(KBLU "1 - Jogar\n" RESET);
-	printf(KBLU "2 - Sair\n" RESET);
-	do { 
-		a = getch_char(); 
-		if(a == '1' || a == '2') break;
-	}while(1);
-	if(a == '1') {
-	 	game_on();
-		tela_fim();
-	}
+int main() {
+	char opt;
+	
+	start();
+	do {
+		show_menu();
+		do { 
+			opt = read_input();
+		}while(opt != '1' && opt != '2');
+		if(opt == '1') {
+		 	game_on();
+		}
+	} while(opt != '2');
 	return 0;
 }
