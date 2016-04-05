151071a477146808c66ac13dc4fecfec1a621a14
HectorID <hectormargittay@live.com>
2016-04-05 11:57:26 -0300

    Added files via upload

--- tetris.c
+++ tetris.c
@@ -3,7 +3,7 @@
 //	André Luiz de Moura Ramos Bittencourt         |
 //	Claudio Segala Rodrigues Silva Filho          |
 //	Anne Caroline de Assis Pereira                |
-//	Hector Marggitay                              |
+//	Hector Rocha Margittay                        |
 // _______________________________________________|
 
 
@@ -27,21 +27,25 @@
 #define KMAG  "\x1b[35m"
 #define KCYN  "\x1b[36m"
 #define KWHT  "\x1b[37m"
+#define HORIZONTAL 0
+#define VERTICAL 1
+#define VAZIO 0
 
 // Structs -------------------------------------------------------------------------------------------------------------------------------------------
 
 typedef struct{
-	int field[25][15], points, over;
+	int field[25][15], points;
 	double duration;
 } tetris;
 
 typedef struct{
-	int position, size, x, y;
+	int color, direction, size, x, y;
 } block;
 
 // Variaveis Globais ---------------------------------------------------------------------------------------------------------------------------------
 
 tetris game;
+block currentBlock;
 int next_round = 1;
 
 // Funcoes -------------------------------------------------------------------------------------------------------------------------------------------
@@ -68,10 +72,10 @@ int getch(void) {
 }
 
 char getch_char(void) {
-  int ch = getch;
+  int ch = getch();
   if(ch >= 0 && ch < 128)
 	  return (char) ch;
-  retunr 0;
+  return 0;
 }
 
 void enter(){
@@ -115,57 +119,46 @@ void show_instructions(){
 	printf(" Use as teclas a, s e d (pode usar CapsLock) para mover as pecas\n");
 	usleep(4000000);
 }
+ 
+//MODULO BLOCO___________________________________________________________________________________________
+int obstaculo(int dx, int dy) {
+	int i;
+	
+	for(i = 0 ; i < currentBlock.size ; i++) {
+		if(currentBlock.y + dy >= 0 &&
+		   currentBlock.y + dy < 25 &&
+		   currentBlock.x + dx >= 0 &&
+		   currentBlock.x + dx < 15 0) {
+			if(game.field[currentBlock.y+dy][currentBlock.x + dx] != VAZIO) {
+		
+			}
+		}
+		else 
+			return 0;
+	}
 
-void fall(char opt){
-	if(opt == 's'){ /* move para baixo */ 
-		if( obstaculo() ){
-			if(currentBlock.position == VERTICAL){ /*  */
-				
-			}
-			else{
-				
-			}
-		}
-		else{
-			next_round = 1;
-		}
+	return 1;
+}
+
+void move(char opt){
+	if(opt == 's' || opt == 'S'){ /* move para baixo */ 
+	
 	}
-	else if(opt == 'd'){ /* move para direita */ 
-		if( obstaculo() ){
-			if(currentBlock.position == VERTICAL){ /*  */
-				
-			}
-			else{
-				
-			}
-		}
+	else if(opt == 'd' || opt == 'D'{ /* move para direita */ 
+
 	}
-	else if(opt == 'a'){ /* move para esquerda */ 
-		if( obstaculo() ){
-			if(currentBlock.position == VERTICAL){ /*  */
-				
-			}
-			else{
-				
-			}
-		}
+	else if(opt == 'a' || opt == 'S'){ /* move para esquerda */ 
+
 	}
 }
 
 void cut_this_line(int x){
-	for (int i = 0; i < 15; ++i){
-		game.field[x][i] = 0;
-	}
-	for (int i = x; i >= 0; --i){
-		for (int j = 0; j < 25; ++j){
-			game.field[i][j] = game.field[i+1][j];
-		}
-	}
-
-
+	for (int i = x; i >= 5; --i)
+		for (int j = 0; j < 15; ++j)
+			game.field[i][j] = game.field[i-1][j];
 }
 
-/* verifica se existe alguma linha completa */
+
 int any_complete_line(){
 	int i, j, p = 0, cnt;
 	for(i = 0; i < 25; i++){
@@ -181,7 +174,9 @@ int any_complete_line(){
 	return p;
 }
 
-void show_table(){
+//MODULO TELA________________________________________________________________________________________
+
+void show_field(){
 	int i, j;
 	limpa_tela_legal();
     printf(" Pontuacao: %d\n\n", game.points++);
@@ -200,45 +195,57 @@ void show_table(){
     usleep(10000);
 }
 
-void start(){
-	char opt;
-	block currentBlock;
-	do{
-		if(next_round == 1){
-			currentBlock.position = rand() % 2;
-			currentBlock.size = rand() % 3 + 3;
-			putBlock(currentBlock); /* chama a primeira peca */
-			next_round = 0;
-		}
-		limpa_tela_legal();
-		show_table();
-		do{ opt = getch_char(); }
-		while( valido(opt) );
-		fall(opt);
-		usleep(800000);
-		game.points += any_complete_line();
-	}while(game.over);
-}
+// MODULO ENGINE_________________________________________________________________________________
 
 void prepare_field(){
 	memset(game.field, 0, sizeof(int)*15*25);
-	game.over = 1;
 	game.points = 0;
+}
+
+int gameOver(){
+	if(currentBlock.y < 5) return 0;
+	return 1;
+}
+
+void putBlock(){
+	currentBlock.color = rand()%4;
+	currentBlock.direction = rand() % 2;
+	currentBlock.size = rand() % 3 + 3;
+	currentBlock.y = 0;
+	currentBlock.x = rand()%15-(currentBlock.position?0:currentBlock.size);
+	/* implementar desenha na matriz */
+}
+
+char input() {
+	char opt;
+	do { opt = getch_char(); }
+	while( valido(opt) );
+	return opt;
 }
 
 void game_on(){
 	clock_t t_begin = clock() / CLOCKS_PER_SEC, t_end;
 	prepare_field();
 	show_instructions();
-	start();
+	do{
+		if(next_round == 1){
+			putBlock(); 
+			next_round = 0;
+		}
+		limpa_tela_legal();
+		show_field();
+		move(input());
+		usleep(800000);
+		game.points += any_complete_line();
+	}while(gameOver(););
 	t_end = clock() / CLOCKS_PER_SEC;
 	/* De acordo com o dito pelo IME em uma de suas apostilas, esse metodo so consegue guardar ate 35 minutos*/
 	game.duration = t_end - t_begin;
-	tela_fim();
 }
 
-/*Hector faz essa parte*/
-void menu(){
+int main(){
+	srand( time(NULL) );
+	tela_inicio();
 	char a;
 	printf(KBLU "1 - Jogar\n" RESET);
 	printf(KBLU "2 - Sair\n" RESET);
@@ -246,12 +253,9 @@ void menu(){
 		a = getch_char(); 
 		if(a == '1' || a == '2') break;
 	}while(1);
-	if(a == '1')	game_on();
-}
-
-int main(){
-	srand( time(NULL) );
-	tela_inicio();
-	menu();
+	if(a == '1') {
+	 	game_on();
+		tela_fim();
+	}
 	return 0;
 }
