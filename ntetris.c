5d3b7b34325b5121bace50432b48e8cc3742b8e2
HectorID <hectormargittay@live.com>
2016-04-13 19:22:08 -0300

    Protótipo com Ncurses funcional

--- ntetris.c
+++ ntetris.c
@@ -56,21 +56,48 @@ void set_color() {
 	init_pair(4, COLOR_BLUE, COLOR_BLACK);
 	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
 	init_pair(6, COLOR_CYAN, COLOR_BLACK);
+<<<<<<< HEAD
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+	init_pair(8, COLOR_BLACK, COLOR_WHITE);
+}
+=======
 
+>>>>>>> f2dfda3a94d3383354c9aabc5f4f7e4c4eeda190
 
 void show_title(){
+	int i;
 	clear();
 	move(0,0);
+<<<<<<< HEAD
+	printw(" _____   _____   _____   _____   _   _____  \n");
+	printw("|_|_|_| |_|_|_| |_|_|_| |_|_|_| |_| |_|_|_| \n");
+	printw("  |_|   |_|___    |_|   |_|     |_| |_|___  \n");
+	printw("  |_|   |_|_|_|   |_|   |_|     |_| |_|_|_| \n");
+	printw("  |_|   |_|___    |_|   |_|     |_|  ___|_| \n");
+	printw("  |_|   |_|_|_|   |_|   |_|     |_| |_|_|_| \n\n");
+	printw("      Aperte [ENTER] para continuar\n");
+	for(i = 0; i < 6; i++) {
+		mvchgat(i, 0, 8, A_BOLD, 1, NULL);
+		mvchgat(i, 8, 8, A_BOLD, 6, NULL);
+		mvchgat(i, 16, 8, A_BOLD, 1, NULL);
+		mvchgat(i, 24, 8, A_BOLD, 2, NULL);
+		mvchgat(i, 32, 4, A_BOLD, 5, NULL);
+		mvchgat(i, 36, 8, A_BOLD, 3, NULL);
+	}
+=======
 	printw(" _____   _____   _____   _____   _   _____ \n");
 	printw("|_|_|_| |_|_|_| |_|_|_| |_|_|_| |_| |_|_|_|\n");
 	printw("  |_|   |_|___    |_|   |_|     |_| |_|___ \n");
 	printw("  |_|   |_|_|_|   |_|   |_|     |_| |_|_|_|\n");
 	printw("  |_|   |_|___    |_|   |_|     |_|  ___|_|\n");
 	printw("  |_|   |_|_|_|   |_|   |_|     |_| |_|_|_|\n");
+>>>>>>> f2dfda3a94d3383354c9aabc5f4f7e4c4eeda190
 	refresh();
 }
 
 void show_menu() {
+	clear();
+	attron(A_BOLD | COLOR_PAIR(4));
 	printw(" ________________\n");
 	printw("|                |\n");
 	printw("| [1][  Jogar  ] |\n");
@@ -79,41 +106,106 @@ void show_menu() {
 	printw("|                |\n");
 	printw("| [2][  Sair   ] |\n");
 	printw("|________________|\n");
+	attroff(A_BOLD | COLOR_PAIR(4));
+	mvchgat(2, 8, 5, A_BOLD, 7, NULL);
+	mvchgat(6, 8, 4, A_BOLD, 1, NULL);
+	refresh();
 }
 
 void show_instructions(){
-	printw(" Use as teclas [A], [S] e [D] (pode usar CapsLock) para mover as pecas\n\n");
-	printw("                   Aperte [ENTER] para continuar\n");
+	clear();
+	printw(" Use as teclas [<], [V] e [>] para mover as pecas\n\n");
+	printw("           Aperte [ENTER] para continuar\n");
+	refresh();
+}
+
+void create_border() {
+	int x, y;
+	
+	clear();
+	attron(COLOR_PAIR(8));
+	for(y = 0; y < ALTURA + 3; y++)
+		for(x = 0; x < (LARGURA * 2) + 2; x++)
+			mvaddch(y, x, ' ');
+	mvprintw(0, 1, "Pontuacao:");
+	attroff(COLOR_PAIR(8));
 }
 
 void show_field(){
 	int x, y;
-    printw(KBLC BGWHT" Pontuacao: %d                    \n" RESET, game.points);
-    for(x = 0; x < LARGURA+2; x++)
-    	printw(BGWHT "  ");
-    printw(RESET "\n");
-    for(y = 0; y < ALTURA; y++){
-    	printw(BGWHT "  ");
+	create_border();
+	attron(COLOR_PAIR(8));
+	mvprintw(0, 11, "%07d", game.points);
+	attroff(COLOR_PAIR(8));
+    for(y = 0; y < ALTURA; y++)
 		for(x = 0; x < LARGURA; x++) {
+			move(y + 2, (2 * x) + 1);
+			attron(A_NORMAL);
 			if(game.field[x][y] == VAZIO) {
+				attron(COLOR_PAIR(7));
 				if(y == LIMITE)	
-					printw(KWHT BGBLC "--");
+					printw("--");
 				else 
-					printw(BGBLC "  ");
+					printw("  ");
+				attroff(COLOR_PAIR(7));
 			}
 			else {
+<<<<<<< HEAD
+				switch(game.field[x][y]) {
+					case 1:	attron(COLOR_PAIR(1));
+							break;
+					case 2:	attron(COLOR_PAIR(2));
+							break;
+					case 3: attron(COLOR_PAIR(3));
+							break;
+					case 4:	attron(COLOR_PAIR(4));
+							break;
+					case 5:	attron(COLOR_PAIR(5));
+							break;
+					case 6:	attron(COLOR_PAIR(6));
+							break;
+					case 7:	attron(COLOR_PAIR(7));
+							break;
+				}
+				attron(A_BOLD);
+				printw("[]");
+				attroff(A_BOLD);
+=======
 				attr_set(A_BOLD | COLOR_SET(game.field[x][y])));
 				printw("[]");
+>>>>>>> f2dfda3a94d3383354c9aabc5f4f7e4c4eeda190
 			}
 		}
-		printw(BGWHT "  \n" RESET);
-    }
-    for(x = 0; x < LARGURA+2; x++)
-    	printw(BGWHT "  ");
-    printw(RESET "\n");
+	refresh();
 }
 
 void show_end(){
+<<<<<<< HEAD
+	clear();
+	refresh();
+	attrset(COLOR_PAIR(1));
+	printw("Voce fez %d pontos em %d minutos e %d segundos\n", game.points, (int)(game.duration/60), (int)(game.duration-(game.duration/60)));
+	refresh();
+	usleep(2000000);
+	attrset(COLOR_PAIR(2));
+	printw("Parabens por ter chegado tao longe jovem padawan\n");
+	refresh();
+	usleep(2000000);
+	printw("Todo seu progresso foi inutil pois nao guardamos ranking ainda\n" );
+	refresh();
+	usleep(2000000);
+	printw("Sinta-se livre de baixar um app de tetris decente\n");
+	refresh();
+	usleep(2000000);
+	printw("Feito por: programadores escravos da bolivia\n");
+	refresh();
+	usleep(2000000);
+	printw("Fazer o que?\n");
+	printw("¯\\_(^-^)_/¯\n");
+	refresh();
+	attrset(A_NORMAL);
+	printw("      Aperte [ENTER] para continuar\n");
+=======
 	attrset(COLOR_SET(1));
 	printw("Voce fez %d pontos em %d minutos e %d segundos\n"  RESET, game.points, (int)(game.duration/60), (int)(game.duration-(game.duration/60)));
 	usleep(2000000);
@@ -128,6 +220,7 @@ void show_end(){
 	usleep(2000000);
 	printw("Fazer o que?\n");
 	printw(" ¯\\_(ツ)_/¯\n");
+>>>>>>> f2dfda3a94d3383354c9aabc5f4f7e4c4eeda190
 }
 
 // MODULO BLOCO_______________________________________________________________________________________________________________________________________
@@ -199,7 +292,7 @@ int collision(int deslocX, int deslocY) {
 	return 0;
 }
 
-void move_block(char opt){
+void move_block(int opt){
 	int newX = currentBlock.x, newY = currentBlock.y;
 	
 	if(opt == KEY_DOWN){ /* move para baixo */ 
@@ -272,14 +365,14 @@ void get_enter() {
 	while(getch()!='\n');
 }
 
-void start_clock() {
-	clock_gettime(CLOCK_REALTIME, &(game.t_start));
-}
-
 void set_game(){
 	srand(time(NULL));
 	memset(game.field, VAZIO, sizeof(int)*LARGURA*ALTURA);
 	game.points = 0;
+}
+
+void start_clock() {
+	clock_gettime(CLOCK_REALTIME, &(game.t_start));
 }
 
 int game_over(){
@@ -301,6 +394,7 @@ void game_on(){
 	set_game();
 	show_instructions();
 	get_enter();
+	create_border();
 	start_clock();
 	do{
 		new_block();
